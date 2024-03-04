976748848c20f18edca5ffcbccd0203f5a611ae0
José Alvarez <jb70532@gmail.com>
2024-03-03 21:02:35 -0300

    proveedor.sql agregada a la carpeta doc/sql

---
+++ doc/sql/proveedor.sql
@@ -0,0 +1,90 @@
+-- LISTAR CLIENTE POR ID
+CREATE OR REPLACE FUNCTION public.func_categoria_r(code integer)
+    RETURNS TABLE(result json)
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+	RETURN QUERY
+	SELECT ROW_TO_JSON(r)
+	FROM (
+		SELECT
+		c."ID",
+		c.categoria
+		FROM tm_categoria c
+		INNER JOIN tm_sucursal s ON c.sucursal = s."ID"
+		WHERE
+		c."ID"   = code
+		AND
+		c.estado = TRUE
+	)r;
+END;
+$function$
+
+-- LISTAR CLIENTE POR EMPRESA
+CREATE OR REPLACE FUNCTION public.func_cliente_empresa(emp integer)
+    RETURNS TABLE(result json)
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+	RETURN QUERY
+	SELECT ROW_TO_JSON(r)
+	FROM(
+	SELECT
+	c."ID",
+	c.cliente,
+	c.ruc,
+	c.telefono,
+	c.direccion,
+	c.correo
+	FROM tm_cliente c
+	INNER JOIN tm_empresa e ON c.empresa = e."ID"
+	WHERE
+	c.empresa = emp
+	AND
+	c.estado  = TRUE
+	)r;
+END;
+$function$
+
+-- CREAR CLIENTE
+CREATE OR REPLACE FUNCTION public.func_cliente_c(emp integer, cli character varying, ruc character varying, tlf character varying, direc character varying, email character varying)
+    RETURNS void
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+	INSERT INTO tm_cliente(empresa,cliente, ruc, telefono, direccion, correo, creacion, estado)
+	VALUES(emp,UPPER(cli),ruc,tlf,UPPER(direc),email,now(),TRUE );
+END;
+$function$
+
+-- ACTUALIZAR SUCURSAL
+CREATE OR REPLACE FUNCTION public.func_cliente_u(id integer, emp integer, cli character varying, r character varying, tlf character varying, direc character varying, email character varying)
+    RETURNS void
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+	UPDATE tm_cliente
+	SET
+	empresa   = emp,
+	cliente   = upper(cli),
+	ruc 	  = r,
+	telefono  = tlf,
+	direccion = upper(direc),
+	correo    = email
+	WHERE
+	"ID" = id;
+END;
+$function$
+
+-- ELIMINAR SUCURSAL
+CREATE OR REPLACE FUNCTION public.func_cliente_d(id integer)
+    RETURNS void
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+	UPDATE tm_cliente c
+	SET estado = FALSE
+	WHERE
+	c."ID"     = id;
+END;
+$function$
--- model/CLIENTES.php
+++ model/CLIENTES.php
@@ -1,47 +1,47 @@
 <?php
-    class Clientes extends Conectar{
-        // LISTAR CLIENTES x COMPANIA
-        public function get_clientes_x_cmp($cmp){
+    class Cliente extends Conectar{
+        // LISTAR CLIENTE x EMPRESA
+        public function get_cliente_x_cmp($emp){
             $conectar = parent::Conexion();
-            $sql   = "SELECT func_clientes_sucursal(:cmp)";
+            $sql   = "SELECT func_cliente_empresa(:cmp)";
             $query = $conectar->prepare($sql);
-            $query->bindValue(':cmp', $cmp, PDO::PARAM_INT);
+            $query->bindValue(':emp', $emp, PDO::PARAM_INT);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
-        // INSERTAR CLIENTES
-        public function get_clientes_create($cmp,$clientes,$ruc){
+        // INSERTAR CLIENTE
+        public function get_cliente_create($cmp,$cliente,$ruc){
             $conectar = parent::Conexion();
-            $sql   = "SELECT func_clientes_c(:cmp, :clientes, :ruc)";
+            $sql   = "SELECT func_cliente_c(:cmp, :cliente, :ruc)";
             $query = $conectar->prepare($sql);
             $query->bindValue(':cmp', $cmp, PDO::PARAM_INT);
-            $query->bindValue(':cod', $clientes, PDO::PARAM_STR);
+            $query->bindValue(':cod', $cliente, PDO::PARAM_STR);
             $query->bindValue(':ruc', $ruc, PDO::PARAM_STR);
             $query->execute();
         }
-        // LISTAR CLIENTES X ID
-        public function get_clientes_read($cod){
+        // LISTAR CLIENTE X ID
+        public function get_cliente_read($cod){
             $conectar = parent::Conexion();
-            $sql   = "SELECT func_clientes_r(:cod)";
+            $sql   = "SELECT func_cliente_r(:cod)";
             $query = $conectar->prepare($sql);
             $query->bindValue(':cod', $cod, PDO::PARAM_INT);
             $query->execute();
             return $query->fetchAll(PDO::FETCH_ASSOC);
         }
-        // ACTUALIZAR CLIENTES
-        public function get_clientes_update($cod,$clientes,$ruc){
+        // ACTUALIZAR CLIENTE
+        public function get_cliente_update($cod,$cliente,$ruc){
             $conectar = parent::Conexion();
-            $sql   = "SELECT func_clientes_u(:cod,:clientes,:ruc)";
+            $sql   = "SELECT func_cliente_u(:cod,:cliente,:ruc)";
             $query = $conectar->prepare($sql);
             $query->bindValue(':cod', $cod, PDO::PARAM_INT);
-            $query->bindValue(':clientes', $clientes, PDO::PARAM_STR);
+            $query->bindValue(':cliente', $cliente, PDO::PARAM_STR);
             $query->bindValue(':ruc', $ruc, PDO::PARAM_STR);
             $query->execute();
         }
-        // ELIMINAR CLIENTES
-        public function get_clientes_delete($cod){
+        // ELIMINAR CLIENTE
+        public function get_cliente_delete($cod){
             $conectar = parent::Conexion();
-            $sql   = "SELECT func_clientes_d(:cod)";
+            $sql   = "SELECT func_cliente_d(:cod)";
             $query = $conectar->prepare($sql);
             $query->bindValue(':cod', $cod, PDO::PARAM_INT);
             $query->execute();
